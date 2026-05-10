3e2bdd310ff79010fe98cb05613a23dda43be985
shahan010100 <shahan010100@gmail.com>
2026-05-10 17:55:24 +0530

    fix ui rendering

--- src/render.js
+++ src/render.js
@@ -2,7 +2,6 @@ import { getWeather } from "./fetch";
 
 let wData;
 
-
 const form=document.querySelector('form');
 const search=document.querySelector('#search');
 const loading=document.querySelector('.loading')
@@ -21,26 +20,33 @@ form.addEventListener('submit', async (e)=>{
     if(wData)
     {
         const temp=selected.querySelector(".temp>span:nth-child(2)");
+        temp.textContent="";
         temp.textContent=wData.days[i].temp;
 
         const condition=selected.querySelector(".temp>span:nth-child(3)");
+        condition.textContent="";
         condition.textContent=wData.days[i].conditions;
 
         const sunrise=selected.querySelector(".other .sun>span:first-child");
+        sunrise.textContent="Sunrise:";
         sunrise.textContent+=wData.days[i].sunrise;
 
         const sunset=selected.querySelector(".other .sun>span:last-child");
+        sunset.textContent="Sunset:";
         sunset.textContent+=wData.days[i].sunset;
 
         const humidity=selected.querySelector(".other .humidity>span:first-child");
+        humidity.textContent="Humidity:";
         humidity.textContent+=wData.days[i].humidity;
 
         const dew=selected.querySelector(".other .humidity>span:last-child");
+        dew.textContent="Dew:"
         dew.textContent+=wData.days[i].dew;
 
         if(i!=0 && i!=1)
         {
             const date=selected.querySelector(".temp>span:first-child");
+            date.textContent="";
             date.textContent=wData.days[i].datetime;
         }
 
